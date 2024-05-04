defb73032ca40304585f9d747b6c229f04f73fe4
Linkiddrak <114172627+Linkiddrak@users.noreply.github.com>
2024-05-04 12:17:28 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,24 +16,24 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V12' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.botname = 'Darki Bot MD V12' //ur bot name
+global.ownernumber = '22871255034' //ur owner number
+global.ownername = '❄️Raizen Linkid' //ur owner name
 global.websitex = "https://youtu.be/tLq8_qOOGFI"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
-global.themeemoji = '🪀'
+global.themeemoji = '❄️'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD12' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "❄️Raizen"
+global.creator = "22871255034@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["22871255034"] // Premium User
 global.hituet = 0
 
 //bot sett
-global.typemenu = 'v10' // menu type 'v1' => 'v10'
-global.typereply = 'v4' // reply type 'v1' => 'v4'
+global.typemenu = 'v1' // menu type 'v1' => 'v10'
+global.typereply = 'v1' // reply type 'v1' => 'v4'
 global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
 global.welcome = false //welcome/left in groups
